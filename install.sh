755557863c46204ff21ba0222460a8d98a7e0388
Clement Bolin <clement.bolin@epitech.eu>
2020-11-08 12:40:16 +0100

    add(master): install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "You must run this with superuser priviliges.  Try \"sudo ./install.sh\"" 2>&1
+    exit 1
+else
+    echo "Installing btop..."
+fi
+
+pwd = $PWD
+
+function install() {
+    if [[ ! -d /usr/local/lib/node_modules ]]
+    then
+        mkdir /usr/local/lib/node_modules
+    fi
+    npm i --save
+    cp ../weatherCLI /usr/local/lib/node_modules
+    cd /usr/local/lib/node_modules/weatherCLI/
+    cd /usr/local/bin
+    ln -s /usr/local/lib/node_modules/weatherCLI/bin/weatherCLI weatherCLI
+    cd $pwd
+    echo "weatherCLI install on your system"
+}
+
+install
